91f79c757e6c8e62302f17d0dcee592af20608cd
Abhijeet Verma <vickyverma4u@gmail.com>
2023-09-14 22:21:01 +0530

    add .env support

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,14 @@
 import { join } from 'path';
-import AutoLoad, {AutoloadPluginOptions} from '@fastify/autoload';
+import AutoLoad, { AutoloadPluginOptions } from '@fastify/autoload';
 import { FastifyPluginAsync, FastifyServerOptions } from 'fastify';
 
-export interface AppOptions extends FastifyServerOptions, Partial<AutoloadPluginOptions> {
+import 'dotenv/config';
 
-}
+export interface AppOptions extends FastifyServerOptions, Partial<AutoloadPluginOptions> {}
 // Pass --options via CLI arguments in command to enable these options.
-const options: AppOptions = {
-}
+const options: AppOptions = {};
 
-const app: FastifyPluginAsync<AppOptions> = async (
-    fastify,
-    opts
-): Promise<void> => {
+const app: FastifyPluginAsync<AppOptions> = async (fastify, opts): Promise<void> => {
   // Place here your custom code!
 
   // Do not touch the following lines
@@ -22,17 +18,16 @@ const app: FastifyPluginAsync<AppOptions> = async (
   // through your application
   void fastify.register(AutoLoad, {
     dir: join(__dirname, 'plugins'),
-    options: opts
-  })
+    options: opts,
+  });
 
   // This loads all plugins defined in routes
   // define your routes in one of these
   void fastify.register(AutoLoad, {
     dir: join(__dirname, 'routes'),
-    options: opts
-  })
-
+    options: opts,
+  });
 };
 
 export default app;
-export { app, options }
+export { app, options };
